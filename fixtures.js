f5d39b0855a64631e0000dedd4c0c6c6a80e46b9
Davismeru <davismeru911@gmai.com>
2024-11-25 13:44:52 +0300

    initial commit

---
+++ fixtures.js
@@ -0,0 +1,291 @@
+// epl
+export const epl_teams = [
+  {
+    name: "Arsenal",
+    logo: "/logos/arsenal.png",
+    index: 0,
+  },
+  {
+    name: "Aston Villa",
+    logo: "/logos/aston.png",
+    index: 1,
+  },
+  {
+    name: "AFC Bournemouth",
+    logo: "/logos/afc.png",
+    index: 2,
+  },
+  {
+    name: "Brentford",
+    logo: "/logos/brentford.png",
+    index: 3,
+  },
+  {
+    name: "Brighton & Hove Albion",
+    logo: "/logos/brighton.png",
+    index: 4,
+  },
+  {
+    name: "Chelsea",
+    logo: "/logos/chelsea.png",
+    index: 5,
+  },
+  {
+    name: "Crystal Palace",
+    logo: "/logos/crystal.png",
+    index: 6,
+  },
+  {
+    name: "Everton",
+    logo: "/logos/everton.png",
+    index: 7,
+  },
+  {
+    name: "Fulham",
+    logo: "/logos/fulham.png",
+    index: 8,
+  },
+  {
+    name: "Ipswich Town",
+    logo: "/logos/ipswich.png",
+    index: 9,
+  },
+  {
+    name: "Leicester City",
+    logo: "/logos/leicester.png",
+    index: 10,
+  },
+  {
+    name: "Liverpool",
+    logo: "/logos/liverpool.png",
+    index: 11,
+  },
+  {
+    name: "Manchester City",
+    logo: "/logos/manchester_1.png",
+    index: 12,
+  },
+  {
+    name: "Manchester United",
+    logo: "/logos/manchester.png",
+    index: 13,
+  },
+  {
+    name: "Newcastle United",
+    logo: "/logos/newcastle.png",
+    index: 14,
+  },
+  {
+    name: "Nottingham Forest",
+    logo: "/logos/nottingham.png",
+    index: 15,
+  },
+  {
+    name: "Southampton",
+    logo: "/logos/southampton.png",
+    index: 16,
+  },
+  {
+    name: "Tottenham Hotspur",
+    logo: "/logos/tottenham.png",
+    index: 17,
+  },
+  {
+    name: "West Ham United",
+    logo: "/logos/west.png",
+    index: 18,
+  },
+  {
+    name: "Wolverhampton Wanderers",
+    logo: "/logos/wolverhampton.png",
+    index: 19,
+  },
+];
+
+// la liga
+export const la_liga_teams = [
+  { index: 0, name: "Barcelona", logo: "/logos/barcelona.png" },
+  { index: 1, name: "Real Madrid", logo: "/logos/real.png" },
+  { index: 2, name: "Atletico Madrid", logo: "/logos/attletico.png" },
+  { index: 3, name: "Sevilla", logo: "/logos/sevilla.png" },
+  { index: 4, name: "Real Sociedad", logo: "/logos/sociedad.png" },
+  { index: 5, name: "Real Betis", logo: "/logos/betis.png" },
+  { index: 6, name: "Athletic Club", logo: "/logos/athletic.png" },
+  { index: 7, name: "Villarreal", logo: "/logos/villarreal.png" },
+  { index: 8, name: "Valencia", logo: "/logos/valencia.png" },
+  { index: 9, name: "Osasuna", logo: "/logos/osasuna.png" },
+  { index: 10, name: "Mallorca", logo: "/logos/mallorca.png" },
+  { index: 11, name: "Celta Vigo", logo: "/logos/celta.png" },
+  { index: 12, name: "Girona", logo: "/logos/girona.png" },
+  { index: 13, name: "Getafe", logo: "/logos/getafe.png" },
+  { index: 14, name: "Rayo Vallecano", logo: "/logos/rayo.png" },
+  { index: 15, name: "Las Palmas", logo: "/logos/palmas.png" },
+  { index: 16, name: "Espanyol", logo: "/logos/espanyol.png" },
+  { index: 17, name: "Alaves", logo: "/logos/alaves.png" },
+  { index: 18, name: "Leganes", logo: "/logos/leganes.png" },
+  { index: 19, name: "Valladolid", logo: "/logos/valladolid.png" },
+];
+
+// serie A
+export const serie_a_teams = [
+  { name: "Napoli", logo: "/logos/napoli.png", index: 0 },
+  { name: "Inter Milan", logo: "/logos/inter.png", index: 1 },
+  { name: "Juventus", logo: "/logos/juventus.png", index: 2 },
+  { name: "AC Milan", logo: "/logos/milan.png", index: 3 },
+  { name: "Fiorentina", logo: "/logos/fiorentina.png", index: 4 },
+  { name: "Atalanta", logo: "/logos/atalanta.png", index: 5 },
+  { name: "Lazio", logo: "/logos/lazio.png", index: 6 },
+  { name: "Udinese", logo: "/logos/udinese.png", index: 7 },
+  { name: "Torino", logo: "/logos/torino.png", index: 8 },
+  { name: "AS Roma", logo: "/logos/roma.png", index: 9 },
+  { name: "Empoli", logo: "/logos/empoli.png", index: 10 },
+  { name: "Bologna", logo: "/logos/bologna.png", index: 11 },
+  { name: "Hellas Verona", logo: "/logos/verona.png", index: 12 },
+  { name: "Como", logo: "/logos/como.png", index: 13 },
+  { name: "Cagliari", logo: "/logos/cagliari.png", index: 14 },
+  { name: "Monza", logo: "/logos/monza.png", index: 15 },
+  { name: "Parma", logo: "/logos/parma.png", index: 16 },
+  { name: "Genoa", logo: "/logos/genoa.png", index: 17 },
+  { name: "Lecce", logo: "/logos/lecce.png", index: 18 },
+  { name: "Sassuolo", logo: "/logos/sassuolo.png", index: 19 },
+];
+
+// champions league
+export const champions_league_teams = [
+  { name: "Manchester City", logo: "/logos/manchester.png", index: 0 },
+  { name: "Arsenal", logo: "/logos/arsenal.png", index: 1 },
+  { name: "Liverpool", logo: "/logos/liverpool.png", index: 2 },
+  { name: "Manchester United", logo: "/logos/manchester.png", index: 3 },
+  { name: "Newcastle United", logo: "/logos/newcastle.png", index: 4 },
+  { name: "Real Madrid", logo: "/logos/real.png", index: 5 },
+  { name: "Barcelona", logo: "/logos/barcelona.png", index: 6 },
+  { name: "Atlético Madrid", logo: "/logos/attletico.png", index: 7 },
+  { name: "Sevilla", logo: "/logos/sevilla.png", index: 8 },
+  { name: "Bayern Munich", logo: "/logos/bayern.png", index: 9 },
+  { name: "Borussia Dortmund", logo: "/logos/dortmund.png", index: 10 },
+  { name: "RB Leipzig", logo: "/logos/leipzig.png", index: 11 },
+  { name: "Inter Milan", logo: "/logos/inter.png", index: 12 },
+  { name: "AC Milan", logo: "/logos/milan.png", index: 13 },
+  { name: "Napoli", logo: "/logos/napoli.png", index: 14 },
+  { name: "Benfica", logo: "/logos/benfica.png", index: 15 },
+  { name: "Porto", logo: "/logos/porto.png", index: 16 },
+  { name: "Celtic", logo: "/logos/celtic.png", index: 17 },
+  { name: "Red Star Belgrade", logo: "/logos/redstar.png", index: 18 },
+  { name: "Feyenoord", logo: "/logos/feyenoord.png", index: 19 },
+  { name: "Shakhtar Donetsk", logo: "/logos/shakhtar.png", index: 20 },
+  { name: "Lazio", logo: "/logos/lazio.png", index: 21 },
+  { name: "PSG", logo: "/logos/psg.png", index: 22 },
+  { name: "Galatasaray", logo: "/logos/galatasaray.png", index: 23 },
+  { name: "Copenhagen", logo: "/logos/copenhagen.png", index: 24 },
+  { name: "Antwerp", logo: "/logos/antwerp.png", index: 25 },
+  { name: "Young Boys", logo: "/logos/youngboys.png", index: 26 },
+  { name: "Union Berlin", logo: "/logos/union.png", index: 27 },
+  { name: "Lens", logo: "/logos/lens.png", index: 28 },
+  { name: "Real Sociedad", logo: "/logos/sociedad.png", index: 29 },
+  { name: "Salzburg", logo: "/logos/salzburg.png", index: 30 },
+  { name: "Braga", logo: "/logos/braga.png", index: 31 },
+  { name: "AEK Athens", logo: "/logos/aek.png", index: 32 },
+  { name: "Dinamo Zagreb", logo: "/logos/dinamo.png", index: 33 },
+  { name: "Maccabi Haifa", logo: "/logos/maccabi.png", index: 34 },
+  { name: "Olympiacos", logo: "/logos/olympiacos.png", index: 35 },
+];
+
+// europa league
+export const europa_league_teams = [
+  { name: "Liverpool", logo: "/logos/liverpool.png", index: 0 },
+  { name: "AS Roma", logo: "/logos/roma.png", index: 1 },
+  { name: "Bayer Leverkusen", logo: "/logos/leverkusen.png", index: 2 },
+  { name: "West Ham United", logo: "/logos/west.png", index: 3 },
+  { name: "Real Betis", logo: "/logos/betis.png", index: 4 },
+  { name: "Atalanta", logo: "/logos/atalanta.png", index: 5 },
+  { name: "Brighton & Hove Albion", logo: "/logos/brighton.png", index: 6 },
+  { name: "Freiburg", logo: "/logos/freiburg.png", index: 7 },
+  { name: "Olympiacos", logo: "/logos/olympiacos.png", index: 8 },
+  { name: "Marseille", logo: "/logos/marseille.png", index: 9 },
+  { name: "Sporting CP", logo: "/logos/sporting.png", index: 10 },
+  { name: "Ajax", logo: "/logos/ajax.png", index: 11 },
+  { name: "Rangers", logo: "/logos/rangers.png", index: 12 },
+  { name: "Fenerbahçe", logo: "/logos/fenerbahce.png", index: 13 },
+  { name: "Manchester United", logo: "/logos/manchester.png", index: 14 }, // Added Manchester United
+  { name: "Sparta Prague", logo: "/logos/sparta.png", index: 15 },
+  { name: "Union Saint-Gilloise", logo: "/logos/union.png", index: 16 },
+  { name: "Slavia Prague", logo: "/logos/slavia.png", index: 17 },
+  { name: "Sturm Graz", logo: "/logos/sturm.png", index: 18 },
+  { name: "HJK Helsinki", logo: "/logos/hjk.png", index: 19 },
+  { name: "LASK Linz", logo: "/logos/lask.png", index: 20 },
+  { name: "Toulouse", logo: "/logos/toulouse.png", index: 21 },
+  { name: "Bodo/Glimt", logo: "/logos/bodo.png", index: 22 },
+  { name: "Dinamo Zagreb", logo: "/logos/dinamo.png", index: 23 },
+  { name: "Maccabi Haifa", logo: "/logos/maccabi.png", index: 24 },
+];
+
+// bundesliga
+export const bundesliga_teams = [
+  { name: "Bayern Munich", logo: "/logos/bayern.png", index: 0 },
+  { name: "Borussia Dortmund", logo: "/logos/dortmund.png", index: 1 },
+  { name: "RB Leipzig", logo: "/logos/leipzig.png", index: 2 },
+  { name: "Bayer Leverkusen", logo: "/logos/leverkusen.png", index: 3 },
+  { name: "Union Berlin", logo: "/logos/union.png", index: 4 },
+  { name: "SC Freiburg", logo: "/logos/freiburg.png", index: 5 },
+  { name: "Eintracht Frankfurt", logo: "/logos/frankfurt.png", index: 6 },
+  { name: "VfL Wolfsburg", logo: "/logos/wolfsburg.png", index: 7 },
+  { name: "Mainz 05", logo: "/logos/mainz.png", index: 8 },
+  { name: "Borussia Mönchengladbach", logo: "/logos/gladbach.png", index: 9 },
+  { name: "TSG Hoffenheim", logo: "/logos/hoffenheim.png", index: 10 },
+  { name: "VfB Stuttgart", logo: "/logos/stuttgart.png", index: 11 },
+  { name: "FC Augsburg", logo: "/logos/augsburg.png", index: 12 },
+  { name: "Werder Bremen", logo: "/logos/bremen.png", index: 13 },
+  { name: "1. FC Köln", logo: "/logos/koln.png", index: 14 },
+  { name: "Hertha Berlin", logo: "/logos/hertha.png", index: 15 },
+  { name: "VfL Bochum", logo: "/logos/bochum.png", index: 16 },
+  { name: "Heidenheim", logo: "/logos/heidenheim.png", index: 17 },
+];
+
+// other leagues
+export const other_teams = [
+  { name: "Bayern Munich", logo: "/logos/bayern.png", index: 0 },
+  { name: "Borussia Dortmund", logo: "/logos/dortmund.png", index: 1 },
+  { name: "RB Leipzig", logo: "/logos/leipzig.png", index: 2 },
+  { name: "Bayer Leverkusen", logo: "/logos/leverkusen.png", index: 3 },
+  { name: "Union Berlin", logo: "/logos/union.png", index: 4 },
+  { name: "SC Freiburg", logo: "/logos/freiburg.png", index: 5 },
+  { name: "Eintracht Frankfurt", logo: "/logos/frankfurt.png", index: 6 },
+  { name: "VfL Wolfsburg", logo: "/logos/wolfsburg.png", index: 7 },
+  { name: "Mainz 05", logo: "/logos/mainz.png", index: 8 },
+  { name: "Borussia Mönchengladbach", logo: "/logos/gladbach.png", index: 9 },
+  { name: "TSG Hoffenheim", logo: "/logos/hoffenheim.png", index: 10 },
+  { name: "VfB Stuttgart", logo: "/logos/stuttgart.png", index: 11 },
+  { name: "FC Augsburg", logo: "/logos/augsburg.png", index: 12 },
+  { name: "Werder Bremen", logo: "/logos/bremen.png", index: 13 },
+  { name: "1. FC Köln", logo: "/logos/koln.png", index: 14 },
+  { name: "Hertha Berlin", logo: "/logos/hertha.png", index: 15 },
+  { name: "VfL Bochum", logo: "/logos/bochum.png", index: 16 },
+  { name: "Heidenheim", logo: "/logos/heidenheim.png", index: 17 },
+];
+
+// featured games all leagues
+export const top_games = [
+  {
+    fixture: [epl_teams[13], epl_teams[5]],
+    time: "25th/11/2024",
+    channel: "tnt",
+    status: "upcoming",
+    league: "english premier league",
+  },
+
+  {
+    fixture: [la_liga_teams[4], la_liga_teams[5]],
+    time: "25th/11/2024",
+    channel: "https://blabla.com",
+    status: "upcoming",
+    league: "la liga",
+  },
+
+  {
+    fixture: [champions_league_teams[1], champions_league_teams[6]],
+    time: "27th/11/2024",
+    channel: "https://example.com/match-arsenal-barcelona",
+    status: "scheduled",
+    league: "uefa champions league",
+  },
+];
